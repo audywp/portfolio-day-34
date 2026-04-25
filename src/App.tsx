d5e1f6b0038484a0ad839965baed7de0394134be
Audy Pratama <audy.wisuda@kelaspintar.id>
2026-04-25 13:59:12 +0700

    membuat section hero

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <section id='hero'>
+      <div className='hero-container'>
+        <h1>Ini ceritanya hero section</h1>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam aperiam vitae repellendus tenetur fugiat
+          reiciendis distinctio nostrum et voluptatem adipisci quod alias magni incidunt, blanditiis quas, numquam
+          repudiandae iusto facilis.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default App;
